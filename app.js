0c90e139debd843ad19e5387b27d6abb1f889fdf
JauniKappauni <social.jauni@gmail.com>
2025-10-30 17:05:30 +0100

    feat: Add EJS and Express dependencies; create initial EJS template

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const port = 3000;
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
+});

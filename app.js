44a277935b3d92aee6db02c19ac616a81b1880e5
JauniKappauni <social.jauni@gmail.com>
2025-10-30 17:26:20 +0100

    feat: Update .gitignore to exclude games.db and initialize SQLite database in app.js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const sqlite3 = require("sqlite3");
 const port = 3000;
 const app = express();
+
+const db = new sqlite3.Database("./games.db");
+
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS games (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, link TEXT, category TEXT)"
+  );
+});
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+    res.render("index");
 });
 
 app.listen(port, () => {
